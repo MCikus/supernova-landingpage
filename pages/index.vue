fcb6fb2f368ea8be7ec5fc07e081e723a9f7f853
Marcel Cikus <cikus@constlab.de>
2022-06-19 15:33:27 +0200

    created a Laningpage for the Supernova Hackathon

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<template>
+  <div class=" relative overflow-hidden">
+    <img class="absolute inset-0 w-full h-full object-cover -z-50" src="~/assets/img/bg.png" alt="" />
+
+    <main class="lg:relative w-screen h-screen ">
+      <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+        <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+          <h1 class="text-4xl tracking-tight font-extrabold text-slate-50 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+            <span class="block xl:inline">WEB3 Empowered</span>
+            <span class="block text-slate-50 xl:inline">Marketing Advisor</span>
+          </h1>
+          <p class="mt-3 max-w-md mx-auto text-lg text-slate-200 sm:text-xl md:mt-5 md:max-w-3xl">Get a Marketing Advise from over 200 Marketing Expert, based on your situation, product and target group with an exact guideline how to do your marketing. Fully on Web3.</p>
+          <div class="mt-10 sm:flex sm:justify-center lg:justify-start	">
+            <div class="rounded-md shadow min-w-full	">
+              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-orange-500 hover:bg-orange-600 md:py-4 md:text-lg md:px-10"> Log-In with Internet Identity </a>
+            </div>
+          </div>
+          <div class="mt-10 sm:flex sm:justify-center lg:justify-start w-full	">
+          <div class="mt-3 rounded-md shadow sm:mt-0  min-w-full	">
+              <a href="#" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-slate-600 hover:bg-slate-700 md:py-4 md:text-lg md:px-10"> Create an Internet Identity  </a>
+            </div>
+          </div>
+          <div class="mt-10 sm:flex sm:justify-center lg:justify-start w-full	">
+            <div class="mt-3 rounded-md sm:mt-0 min-w-full	">
+              <a href="#" class="w-full flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-white md:py-4 md:text-lg md:px-10 underline underline-offset-4" > What is an Internet Identity?  </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full ">
+        <img class="inset-0 w-4/6 h-auto my-24 mx-8 invisible md:visible" src="~/assets/img/logos.png" alt="" />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup>
+
+</script>
